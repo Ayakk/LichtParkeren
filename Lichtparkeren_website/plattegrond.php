4ab8268266c26d6a75c770a87a739d23c898b199
DanielDmln <57104964+DanielDmln@users.noreply.github.com>
2020-01-24 15:54:50 +0100

    Update plattegrond.php

--- Lichtparkeren_website/plattegrond.php
+++ Lichtparkeren_website/plattegrond.php
@@ -10,33 +10,29 @@
                 if (nr == 2) {
                     pic = "klikocijfer_2.jpg"
                 }
-                if (nr == 3) {
-                    pic = "klikocijfer_3.jpg"
-                }
                 document.getElementById('myImage').src = pic;
             }
             function openMainMenu(){
             window.location.replace("http://localhost/Lichtparkeren_website/index.php")
         }
         function openLog_Page(){
-            window.location.replace("http://localhost/Lichtparkeren_website/log_page.php")
+            window.location.replace("http://localhost/Lichtparkeren_website/Log_Page.php")
         }
         </script>
+    <body style="background-color:#333333">
         <form>
         <input type="button" value="Hoofdmenu" onclick="openMainMenu()">
     </form>
         <form>
-        <input type="button" value="Log Page" onclick="openLog_page()">
+        <input type="button" value="Log Page" onclick="openLog_Page()">
     </form>
-        <p style="color:white;font-size:200%;text-align:center;"><b>
+        <h1 style="color:white;font-size:300%;text-align:center;"><b>
                 Plattegrond Parkeergarage:
-            </b></p>
-        <img id="myImage" src="klikocijfer_0.jpg" width="100" height="600">
+            </b></h1>
+        <img id="myImage" src="klikocijfer_0.jpg" width="100%" height="600">
         <p style="text-align:center;">
             <button type="button" onclick="etage(0)">BG</button>
             <button type="button" onclick="etage(1)">E1</button>
             <button type="button" onclick="etage(2)">E2</button>
-            <button type="button" onclick="etage(3)">E3</button>
         </p>
     </body>
-
